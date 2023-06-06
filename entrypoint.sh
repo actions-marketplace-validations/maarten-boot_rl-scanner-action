1cd9630a17c5d1eafce6708a8ff92cda57390e01
Maarten Boot <11140490+maarten-boot@users.noreply.github.com>
2023-06-06 10:10:42 +0200

    add config

---
+++ entrypoint.sh
@@ -0,0 +1,99 @@
+#! /bin/bash -l
+
+# we must have the mandatory environment variables:
+# RLSECURE_ENCODED_LICENSE=<base64 encoded license file>
+# RLSECURE_SITE_KEY=<site key>
+
+# we may have the environment variables:
+# RLSECURE_PROXY_SERVER	Optional. Server URL for local proxy configuration.
+# RLSECURE_PROXY_PORT	Optional. Network port for local proxy configuration.
+# RLSECURE_PROXY_USER	Optional. User name for proxy authentication.
+# RLSECURE_PROXY_PASSWORD	Optional. Password for proxy authentication.
+
+# we need a mandatory input path to a single articat we will scan
+# test if the file is readabole and not zero in length
+
+EXIT_FATAL=101
+
+fatal()
+{
+    local msg="$1"
+    echo "status=FATAL: ${msg}" >>${GITHUB_OUTPUT}
+    exit ${EXIT_FATAL}
+}
+
+testInputArtifactToScan()
+{
+    local item="$1"
+
+    [ ! -f "$item" ] && {
+        fatal "file not found: '$item'"
+    }
+
+    [ ! -r "$item" ] && {
+        fatal "file not readable: '$item'"
+    }
+
+    [ ! -s "$item" ] && {
+        fatal "file has zero length: '$item'"
+    }
+
+    INPUT_FILE_TO_SCAN="${item}"
+}
+
+testEnvVarsMandatory()
+{
+    [ -z "${RLSECURE_ENCODED_LICENSE}" ] && {
+        fatal "no value provided for mandatory environment variable: 'RLSECURE_ENCODED_LICENSE'"
+    }
+
+    [ -z "${RLSECURE_SITE_KEY}" ] && {
+        echo "no value provided for mandatory environment variable: 'RLSECURE_SITE_KEY'"
+    }
+}
+
+main()
+{
+    testEnvVarsMandatory
+    testInputArtifactToScan $*
+
+    rm -rf .rl_report
+    mkdir .rl_report
+
+    rl-scan \
+        --package-path=$1 \
+        --report-path=.rl_report \
+        --report-format=all 2>/tmp/2 1>/tmp/1
+    RR=$? # preserve the exit code for later
+
+    # we are only interested in the PASS / FAIL line for the OUTPUT
+    STATUS=$( grep "Scan result:" /tmp/1 )
+    [ -z "$STATUS" ] && {
+        local msg="No scan result found in the scanner command output"
+        echo "::error::$msg"
+        echo "status=$msg"      >>$GITHUB_OUTPUT
+        echo "state=error"      >>$GITHUB_OUTPUT
+        echo "Error: $msg"      >>$GITHUB_STEP_SUMMARY
+
+        cat /tmp/2
+        cat /tmp/1
+        exit 101
+    }
+
+    cat /tmp/1
+    if [ "$RR" != "0" ]
+    then
+        echo "::error::$STATUS"
+        echo "state=failure"    >>$GITHUB_OUTPUT
+    else
+        echo "::notice::$STATUS"
+        echo "state=success"    >>$GITHUB_OUTPUT
+    fi
+
+    echo "status=$STATUS"       >>$GITHUB_OUTPUT
+    echo "$STATUS"              >>$GITHUB_STEP_SUMMARY
+    exit $RR
+}
+
+# we pass all args to the function
+main $*
